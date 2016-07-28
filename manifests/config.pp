c0fcdba71556052ef0bf8caf79c334a264f34b81
Samuel Keeley <samuel.keeley@airbnb.com>
2016-07-27 17:02:20 -0700

    ruby hash no erb

--- manifests/config.pp
+++ manifests/config.pp
@@ -14,9 +14,56 @@ class munki::config {
   $suppress_user_notification     = $munki::suppress_user_notification
   $use_client_cert                = $munki::use_client_cert
 
+
+  $profile = {
+              "PayloadContent" => [
+        {
+               "PayloadContent" => {
+                "ManagedInstalls" => {
+                    "Forced" => [
+                        {
+                            "mcx_preference_settings" => {
+                                      "AdditionalHttpHeaders" => [
+                                    'Authorization: Basic ***REMOVED***'
+                                ],
+                                   "AppleSoftwareUpdatesOnly" => $apple_software_updates_only,
+                                      "ClientCertificatePath" => $client_cert_path,
+                                              "ClientKeyPath" => $client_key_path,
+                                   "DaysBetweenNotifications" => $days_between_notifications,
+                                "InstallAppleSoftwareUpdates" => $install_apple_software_updates,
+                                               "LoggingLevel" => $logging_level,
+                                              "MSULogEnabled" => $msu_log_enabled,
+                                  "SoftwareRepoCACertificate" => $software_repo_ca_cert,
+                                            "SoftwareRepoURL" => $software_repo_url,
+                                    "SoftwareUpdateServerURL" => $software_update_server_url,
+                                   "SuppressUserNotification" => $suppress_user_notification,
+                                       "UseClientCertificate" => $use_client_cert
+                            }
+                        }
+                    ]
+                }
+            },
+               "PayloadEnabled" => true,
+            "PayloadIdentifier" => "MCXToProfile.1dc15df4-d4c4-4b3a-b507-dd8f3b44f093.alacarte.customsettings.2beb4aeb-861b-4000-8c3a-d05117bf5ba7",
+                  "PayloadType" => "com.apple.ManagedClient.preferences",
+                  "PayloadUUID" => "2beb4aeb-861b-4000-8c3a-d05117bf5ba7",
+               "PayloadVersion" => 1
+        }
+    ],
+          "PayloadDescription" => "Included custom settings:\nManagedInstalls",
+          "PayloadDisplayName" => "Settings for Munki",
+           "PayloadIdentifier" => "ManagedInstalls",
+         "PayloadOrganization" => "",
+    "PayloadRemovalDisallowed" => true,
+                "PayloadScope" => "System",
+                 "PayloadType" => "Configuration",
+                 "PayloadUUID" => "1dc15df4-d4c4-4b3a-b507-dd8f3b44f093",
+              "PayloadVersion" => 1
+  }
+
   mac_profiles_handler::manage { 'ManagedInstalls':
     ensure      => present,
-    file_source => template('munki/ManagedInstalls.mobileconfig.erb'),
+    file_source => plist($profile),
     type        => 'template',
   }
 
