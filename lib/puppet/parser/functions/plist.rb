ff00a092f7a120d89a79448408d95125ff15de91
Samuel Keeley <samuel.keeley@airbnb.com>
2016-07-27 17:13:58 -0700

    include plist

---
+++ lib/puppet/parser/functions/plist.rb
@@ -0,0 +1,14 @@
+# ruby hash
+
+require 'puppet/util/plist'
+
+module Puppet::Parser::Functions
+  newfunction(:plist, :type => :rvalue ) do |args|
+
+    hash   = args[0]      || {}
+    format = args[1]      || :xml
+
+    return Puppet::Util::Plist.dump_plist(hash, format)
+
+  end
+end
