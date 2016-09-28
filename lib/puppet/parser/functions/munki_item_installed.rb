2d688c29b8306bc8b6eedeabd7d32dc88f03ad0f
Samuel Keeley <samuel.keeley@airbnb.com>
2016-09-28 10:11:40 -0700

    minor cleanup

--- lib/puppet/parser/functions/munki_item_installed.rb
+++ lib/puppet/parser/functions/munki_item_installed.rb
@@ -8,18 +8,14 @@ module Puppet::Parser::Functions
   newfunction(:munki_item_installed, type: :rvalue, doc: <<-EOS
 Returns true if Munki thinks the item is installed.
     EOS
-   ) do |args|
+             ) do |args|
 
     raise(Puppet::ParseError, 'munki_item_installed(): ' \
     "Wrong number of arguments given (#{args.size} for 1)") if args.size != 1
 
-
     munki_managed_installs = lookupvar('munki_managed_installs')
     # Nothing in here, return false
-    if munki_managed_installs.size == 0
-      return false
-    end
-
+    return false if munki_managed_installs.size.zero?
 
     if munki_managed_installs.include? args[0]
       # If a version has been specified, make sure it's that version installed
