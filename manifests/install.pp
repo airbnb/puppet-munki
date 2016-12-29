b308c93030fb78c5687b13411885cec75c5130bd
Graham Gilbert <graham.gilbert@airbnb.com>
2016-12-29 10:53:21 -0800

    Use the new apple_package

--- manifests/install.pp
+++ manifests/install.pp
@@ -18,66 +18,24 @@ class munki::install {
   # $today - (86400 seconds in a day * $days_before_broken)
   $broken_days_ago = $today - (86400 * $days_before_broken)
 
-  if $days_before_broken != 0 {
-    if (($facts['munki_last_run_unix'] != undef and
-        ($facts['munki_last_run_unix'] < $broken_days_ago or
-        $facts['munki_dir_exists'] == false
-        or $facts['munki_version'] == 'Munki not installed')
-      ) or
-      versioncmp($facts['munki_version'], $munkitools_version) < 0 ) and
-      $facts['munki_running'] == false {
-      # Munki has run before, but it's not run for ages
-
-      # Bin the Puppet receipt
-      exec { "/bin/rm -f /var/db/.puppet_pkgdmg_installed_munkitools-${munkitools_version}":
-      }
-
-      # Forget the real receipts
-      exec {'/usr/sbin/pkgutil --forget com.googlecode.munki.admin':
-        returns => [0, 1]
-      }
-
-      exec {'/usr/sbin/pkgutil --forget com.googlecode.munki.app':
-        returns => [0, 1]
-      }
-
-      exec {'/usr/sbin/pkgutil --forget com.googlecode.munki.core':
-        returns => [0, 1]
-      }
-
-      exec {'/usr/sbin/pkgutil --forget com.googlecode.munki.launchd':
-        returns => [0, 1]
-      }
-
-    }
-
-  }
-
-
-  if versioncmp($facts['munki_version'], $munkitools_version) < 0 {
-    notify{"${facts['munki_version']} is less than ${munkitools_version}": }
-  }
-
-  if (macos_package_installed('com.googlecode.munki.core', $munkitools_version) == false or
+  if $facts['munki_last_run_unix'] != undef and $facts['munki_last_run_unix'] < $broken_days_ago {
+    $force_install = true
+  } elsif (macos_package_installed('com.googlecode.munki.core', $munkitools_version) == false or 
   $facts['munki_dir_exists'] == false or
   $facts['munki_version'] == 'Munki not installed' or
   versioncmp($facts['munki_version'], $munkitools_version) < 0) and
-  $facts['munki_running'] == false
-  {
-    file { "${facts['puppet_vardir']}/packages/munkitools.pkg":
-      ensure  => file,
-      source  => $package_source,
-      mode    => '0644',
-      backup  => false,
-      require => File["${facts['puppet_vardir']}/packages"],
-    }
+  $facts['munki_running'] == false {
+    $force_install = true
+  } else {
+    $force_install = false
+  }
 
-    package { "munkitools-${munkitools_version}":
-      ensure   => installed,
-      provider => pkgdmg,
-      source   => "${facts['puppet_vardir']}/packages/munkitools.pkg",
-      require  => File["${facts['puppet_vardir']}/packages/munkitools.pkg"],
-    }
+  apple_package { 'munkitools':
+    source        => $package_source,
+    version       => $munkitools_version,
+    receipt       => 'com.googlecode.munki.core',
+    installs      => ['/Applications/Managed Software Center.app/Contents/MacOS/Managed Software Center', '/usr/local/munki/managedsoftwareupdate'],
+    force_install => $force_install
   }
 
   # Make sure everything is owned by root
